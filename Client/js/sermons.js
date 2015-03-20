6e716c0ca27956dd873dafb92ce6db8345fd8aef
Matthew Wilson <matt@mattcodes.co.uk>
2015-03-20 00:58:18 +0000

    Adding sermons page and components

---
+++ Client/js/sermons.js
@@ -0,0 +1,68 @@
+var SermonHeaders = React.createClass({
+  render: function() {
+    return (
+        <tr>
+          <th>Day</th>
+          <th>Month</th>
+          <th>Year</th>
+          <th>Speaker</th>
+          <th>Book</th>
+          <th>Verses</th>
+          <th>Subject</th>
+          <th>Type</th>
+          <th>Download Link</th>
+        </tr>
+    );
+  }
+});
+
+
+var SermonTable = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  render: function() {
+
+    var sermons = this.state.data.map(function(sermon) {
+          return (
+            <tr>
+              <td>{sermon.day}</td>
+              <td>{sermon.month}</td>
+              <td>{sermon.year}</td>
+              <td>{sermon.speaker}</td>
+              <td>{sermon.bibleBook}</td>
+              <td>{sermon.verses}</td>
+              <td>{sermon.subject}</td>
+              <td>{sermon.type}</td>
+              <td>{sermon.downloadLink}</td>
+            </tr>
+          );
+    });
+
+    return (
+      <table className="table table-striped">
+        <tbody>
+          <SermonHeaders/>
+          {sermons}
+        </tbody>
+      </table>
+    );
+  }
+});
+
+React.render(
+  <SermonTable url="http://saintfieldbaptist.org.uk/php/GetSermons.php"/>,
+  document.getElementById('sermons')
+);
