1cacf250550e0775df22a7db1387bcffec6ec6f4
Matthew Wilson <matt@mattcodes.co.uk>
2015-03-20 21:29:02 +0000

    Adding audio player component to sermons table

--- Client/js/sermons.js
+++ Client/js/sermons.js
@@ -4,13 +4,74 @@ var SermonHeaders = React.createClass({
         <tr>
           <th>Date</th>
           <th>Speaker</th>
-          <th>Book</th>
-          <th>Verses</th>
+          <th>Reading</th>
           <th>Subject</th>
           <th>Type</th>
           <th>Download Link</th>
         </tr>
     );
+  }
+});
+
+function makeSuffixRegExp(suffix, caseInsensitive) {
+  return new RegExp(
+      String(suffix).replace(/[$%()*+.?\[\\\]{|}]/g, "\\$&") + "$",
+      caseInsensitive ? "i" : "");
+}
+
+var AudioPlayer = React.createClass({
+  getInitialState: function() {
+    return {
+      url : this.props.url,
+      play : false,
+      downloadOnly : false
+    };
+  },
+  componentDidMount: function() {
+    $.ajax({
+      url: 'http://saintfieldbaptist.org.uk/php/ExpandSermonLink.php?link='+this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        var expandedUrl = data.expandedUrl;
+
+        if(!makeSuffixRegExp(".mp3").test(expandedUrl))
+        {
+          this.setState({url: this.state.url, play : this.state.play, downloadOnly : true});
+        }
+
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  listenClicked : function() {
+    this.setState({url: this.state.url, play: true, downloadOnly : this.state.downloadOnly});
+  },
+  render: function() {
+
+    if(this.state.downloadOnly)
+    {
+      return (
+        <a href={this.state.url} className="btn btn-default btn-sm">
+          <span className="glyphicon glyphicon-headphones" aria-hidden="true"></span> Download
+        </a>
+      );
+    } else if(this.state.play) {
+      return (
+        <audio controls>
+          <source src={this.state.url} type="audio/mpeg"></source>
+          Your browser does not support the audio element.
+        </audio>
+      );
+    } else {
+      return (
+        <button type="button" className="btn btn-default btn-sm" onClick={this.listenClicked}>
+          <span className="glyphicon glyphicon-headphones" aria-hidden="true"></span> Listen
+        </button>
+      );
+    }
+
   }
 });
 
@@ -38,11 +99,10 @@ var SermonTable = React.createClass({
             <tr>
               <td>{sermon.day} {sermon.month} {sermon.year}</td>
               <td>{sermon.speaker}</td>
-              <td>{sermon.bibleBook}</td>
-              <td>{sermon.verses}</td>
+              <td>{sermon.bibleBook} {sermon.verses}</td>
               <td>{sermon.subject}</td>
               <td>{sermon.type}</td>
-              <td><a href={sermon.downloadLink}>Download</a></td>
+              <td><AudioPlayer url={sermon.downloadLink}/></td>
             </tr>
           );
     });
