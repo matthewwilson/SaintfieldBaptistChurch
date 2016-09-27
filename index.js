7c01b121845ceb56a60ed7503bfc90c4cd3ba905
Matthew Wilson <matthew@productearthsolutions.com>
2016-09-27 10:46:18 +0100

    Initial commit, moving server to node.js

---
+++ index.js
@@ -0,0 +1,12 @@
+var compression = require('compression');
+var express = require('express');
+var app = express();
+app.use(compression());
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
