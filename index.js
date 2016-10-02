e0a1bdf00a8983af6bf7d56a92b413a5fb28bf64
Matthew Wilson <matt@mattcodes.co.uk>
2016-10-02 16:11:10 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+var path = require('path')
 var compression = require('compression');
 var express = require('express');
 var app = express();
 app.use(compression());
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.set('port', (process.env.PORT || 3000));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.use('/', express.static(path.join(__dirname,'web')));
+
+app.listen(app.get('port'), function () {
+  console.log('SBC started on port: '+app.get('port'));
 });
