4f564a62967e4dd2e3912939b55c2829633afbe3
Matthew Wilson <matt@mattcodes.co.uk>
2018-09-09 16:04:21 +0100

    disabling live stream

--- src/components/live/LivePage.js
+++ src/components/live/LivePage.js
@@ -3,13 +3,24 @@ import './LivePage.css'
 
 
 const LivePage = (props) => {
+  // const contents = (
+  //   <div className="embed-responsive embed-responsive-16by9">
+  //     <iframe title="Live" className="embed-responsive-item" src="https://embed.sermonaudio.com/player/l/saintfieldbaptist/?autoplay=true" allowFullScreen frameBorder="0" scrolling="no"></iframe>
+  //   </div>
+  // );
+
+  const contents = (
+    <div>
+      <br/>
+      <h3 class="text-center">Tonight’s service will not be broadcast live. We are sorry for any inconvenience caused. Please tune in next Sunday morning at 11.30am</h3>
+    </div>
+  );
+
   return (
     <div className="live-page">
       <h1>Watch Live</h1>
       <hr/>
-      <div className="embed-responsive embed-responsive-16by9">
-        <iframe title="Live" className="embed-responsive-item" src="https://embed.sermonaudio.com/player/l/saintfieldbaptist/?autoplay=true" allowFullScreen frameBorder="0" scrolling="no"></iframe>
-      </div>
+      {contents}
     </div>
   )
 }
