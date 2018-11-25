63a32429052d437deffaadcd047392c9fe9d4396
Matthew Wilson <matt@mattcodes.co.uk>
2018-11-25 14:24:27 +0000

    updating bulletin

--- src/components/live/LivePage.js
+++ src/components/live/LivePage.js
@@ -3,18 +3,42 @@ import './LivePage.css'
 
 
 const LivePage = (props) => {
-  // const contents = (
-  //   <div className="embed-responsive embed-responsive-16by9">
-  //     <iframe title="Live" className="embed-responsive-item" src="https://embed.sermonaudio.com/player/l/saintfieldbaptist/?autoplay=true" allowFullScreen frameBorder="0" scrolling="no"></iframe>
-  //   </div>
-  // );
+
+  const style = {
+    position: 'relative',
+    width: '100%',
+    height: 0,
+    paddingBottom: '56.25%'
+  }
+
+  const iFrameStyle = {
+    position: 'absolute',
+    left: 0,
+    top: 0
+  }
 
   const contents = (
-    <div>
-      <br/>
-      <h3 class="text-center">Tonight&#39;s service will not be broadcast live. We are sorry for any inconvenience caused. Please tune in next Sunday morning at 11.30am</h3>
+    <div style={style}>
+      <iframe
+        title="Live"
+        tabIndex="-1"
+        width="100%"
+        height="100%"
+        src="https://embed.sermonaudio.com/player/l/saintfieldbaptist/"
+        style={iFrameStyle}
+        allowFullScreen
+        frameborder="0"
+        scrolling="no">
+      </iframe>
     </div>
   );
+
+  // const contents = (
+  //   <div>
+  //     <br/>
+  //     <h3 class="text-center">Tonight&#39;s service will not be broadcast live. We are sorry for any inconvenience caused. Please tune in next Sunday morning at 11.30am</h3>
+  //   </div>
+  // );
 
   return (
     <div className="live-page">
