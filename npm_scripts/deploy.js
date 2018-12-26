9fbee0503dd005d86b6dd72fc20fe6e7603affcd
Matthew Wilson <matt@mattcodes.co.uk>
2018-12-26 13:49:58 +0000

    updating packages and removing large files

--- npm_scripts/deploy.js
+++ npm_scripts/deploy.js
@@ -1,19 +1,39 @@
+const path = require('path')
 var FtpDeploy = require('ftp-deploy');
 var ftpDeploy = new FtpDeploy();
- 
+
 var config = {
-    user: process.ENV.FTP_USER,
-    password: process.ENV.FTP_PASSWORD,
-    host: process.ENV.FTP_HOST,
+    user: "macbook-instil@saintfieldbaptist.org.uk", //process.ENV.FTP_USER,
+    password: "jrQtqMCTX9YLvjamwQTVhWAV", //process.ENV.FTP_PASSWORD,
+    host: "ftp.saintfieldbaptist.org.uk", //process.ENV.FTP_HOST,
     port: 21,
-    localRoot: __dirname + '/build',
+    localRoot: path.join(__dirname, '..', '/build'),
     remoteRoot: '/',
    	include: ['*', '**/*'],
     deleteRemote: true,
     forcePasv: true
 }
- 
+
+ftpDeploy.on('uploading', function(data) {
+    console.log('Uploading...')
+    console.log('Total files count:' + data.totalFilesCount);
+    console.log('Transferred file count:' + data.transferredFileCount);
+    console.log('File name:' + data.filename);
+});
+
+ftpDeploy.on('uploaded', function(data) {
+  console.log('Uploaded...')
+  console.log('Total files count:' + data.totalFilesCount);
+  console.log('Transferred file count:' + data.transferredFileCount);
+  console.log('File name:' + data.filename);
+});
+
+ftpDeploy.on('upload-error', function (data) {
+  console.log('Upload Error')
+	console.log(data.err);
+});
+
 // use with promises
-await ftpDeploy.deploy(config)
+ftpDeploy.deploy(config)
     .then(res => console.log('finished:', res))
     .catch(err => console.log(err))
