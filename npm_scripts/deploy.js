9002e43dcbdf900d4d442733da975142a0fe045a
Matthew Wilson <matt@mattcodes.co.uk>
2018-12-29 23:06:18 +0000

    sftp deploy

--- npm_scripts/deploy.js
+++ npm_scripts/deploy.js
@@ -1,39 +1,27 @@
 const path = require('path')
-var FtpDeploy = require('ftp-deploy');
-var ftpDeploy = new FtpDeploy();
+const SftpUpload = require('sftp-upload')
 
-var config = {
-    user: process.env.FTP_USER,
-    password: process.env.FTP_PASSWORD,
+var options = {
     host: process.env.FTP_HOST,
-    port: 21,
-    localRoot: path.join(__dirname, '..', '/build'),
-    remoteRoot: '/',
-   	include: ['*', '**/*'],
-    deleteRemote: true,
-    forcePasv: false
+    username: process.env.FTP_USER,
+    password: process.env.FTP_PASSWORD,
+    port: 1050,
+    path: path.join(__dirname, '..', '/build'),
+    remoteDir: 'public_html'
 }
 
-ftpDeploy.on('uploading', function(data) {
-    console.log('Uploading...')
-    console.log('Total files count:' + data.totalFilesCount);
-    console.log('Transferred file count:' + data.transferredFileCount);
-    console.log('File name:' + data.filename);
-});
+sftp = new SftpUpload(options);
 
-ftpDeploy.on('uploaded', function(data) {
-  console.log('Uploaded...')
-  console.log('Total files count:' + data.totalFilesCount);
-  console.log('Transferred file count:' + data.transferredFileCount);
-  console.log('File name:' + data.filename);
-});
+sftp.on('error', function(err) {
+    throw err;
+})
+.on('uploading', function(progress) {
+    console.log('Uploading', progress.file);
+    console.log(progress.percent+'% completed');
+})
+.on('completed', function() {
+    console.log('Upload Completed');
+})
+.upload();
 
-ftpDeploy.on('upload-error', function (data) {
-  console.log('Upload Error')
-	console.log(data.err);
-});
 
-// use with promises
-ftpDeploy.deploy(config)
-    .then(res => console.log('finished:', res))
-    .catch(err => console.log(err))
