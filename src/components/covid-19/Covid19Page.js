86709b44ab47d5ee7f88006e06b1ad8e66ac0b34
Matthew Wilson <matt@mattcodes.co.uk>
2020-07-05 06:20:34 +0100

    adding width

--- src/components/covid-19/Covid19Page.js
+++ src/components/covid-19/Covid19Page.js
@@ -15,10 +15,10 @@ class Covid19Page extends React.Component {
       <Page>
         <PageBanner backgroundColor="#ff5a34" titleColour="white" title="Back to Church"/>
         <div className="ml-3 mr-3">
-          <iframe title="back-to-church" width="560" height="315" src="https://www.youtube.com/embed/LIpYOqCt374" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe title="back-to-church" width="100%" height="315" src="https://www.youtube.com/embed/LIpYOqCt374" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
         <div className="ml-3 mr-3">
-          <iframe title="walkthrough" width="560" height="315" src="https://www.youtube.com/embed/KsdmVh7Lzww" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe title="walkthrough" width="100%" height="315" src="https://www.youtube.com/embed/KsdmVh7Lzww" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
       </Page>
     )
