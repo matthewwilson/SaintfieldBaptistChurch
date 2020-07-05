054e79dee9a439de407b8ea0a6e68ae4bd00add1
Matthew Wilson <matt@mattcodes.co.uk>
2020-07-05 06:21:22 +0100

    top

--- src/components/covid-19/Covid19Page.js
+++ src/components/covid-19/Covid19Page.js
@@ -17,7 +17,7 @@ class Covid19Page extends React.Component {
         <div className="ml-3 mr-3">
           <iframe title="back-to-church" width="100%" height="315" src="https://www.youtube.com/embed/LIpYOqCt374" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
-        <div className="ml-3 mr-3">
+        <div className="ml-3 mr-3 mt-3">
           <iframe title="walkthrough" width="100%" height="315" src="https://www.youtube.com/embed/KsdmVh7Lzww" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
       </Page>
