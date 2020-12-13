5a96e0d233aab7e5c977f2bdc6495bb91655a02e
Matthew Wilson <matthew.wilson@instil.co>
2020-12-13 10:32:56 +0000

    Adding message about face coverings

--- src/components/home/HomePageMeetingTimes.js
+++ src/components/home/HomePageMeetingTimes.js
@@ -9,7 +9,7 @@ const HomePageMeetingTimes = (props) => {
         <div className="home-page-meeting-times">
           <h2>AS A CHURCH WE MEET TOGETHER ON SUNDAYS AT 11.30AM &amp; 6.30PM</h2>
           <br />
-          <h3>We run a baby crèche and children's church during the morning service</h3>
+          <h3>In line with the Northern Ireland Executive Covid-19 guidelines, which state, it is mandatory for all those attending a place of worship to wear a face covering, not only for arrival and exit, but also for the entire time they are in the building (ie: throughout the meeting). Therefore, unless medically exempt, we would kindly ask you to adhere to these new guidelines and wear a face covering while in attendance at any of the services in the church building</h3>
           <br />
         </div>
       </div>
