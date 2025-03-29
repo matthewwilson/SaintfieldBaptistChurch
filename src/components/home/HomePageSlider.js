a04236bc1147e356eff1c809305d2d80ea4fd34e
Matthew Wilson <matthew.wilson@instil.co>
2025-03-29 19:38:22 +0000

    Updating home page slides

--- src/components/home/HomePageSlider.js
+++ src/components/home/HomePageSlider.js
@@ -1,61 +1,72 @@
-import React from 'react';
-import Slider from 'react-slick';
-import $ from 'jquery';
-import HomePageSlide from './HomePageSlide'
+import React from "react";
+import Slider from "react-slick";
+import $ from "jquery";
+import HomePageSlide from "./HomePageSlide";
 import "../../../node_modules/slick-carousel/slick/slick.css";
 import "../../../node_modules/slick-carousel/slick/slick-theme.css";
-import "./HomePageSlider.css"
-
+import "./HomePageSlider.css";
 
 class HomePageSlider extends React.Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       padding: this.getPadding(),
-      slides: [{
-          "type":"GOSPEL MISSION",
-          "title":"WHERE IS GOD?",
-          "subtitle": "The search for reason in a troubled world",
-          "imageUrl":"img/slides/where-is-god.jpeg",
-          "url":"/sermons/series/Where%20is%20God%3F",
-          "backgroundPosition":"center",
-          "buttonText":"LISTEN AGAIN",
-          "internalLink":true,
-        }, {
-          "type":"SUNDAY MORNING SERIES",
-          "title":"An Open Letter to the Church",
-          "subtitle": "Studies in the Book of Titus",
-          "url":"/sermons/series/Titus",
-          "imageUrl":"img/slides/titus.jpeg",
-          "internalLink":true,
-          "buttonText":"LISTEN AGAIN"
-        }, {
-          "type":"CHILDRENS & YOUTH WORK",
-          "title":"What's On",
-          "subtitle": "Term-time activities for tots, tweens and teens",
-          "url":"/about/whats-on",
-          "imageUrl":"img/slides/events.jpg",
-          "internalLink":true,
-          "buttonText":"FIND OUT MORE"
-        }
-      ]
-    }
+      slides: [
+        {
+          type: "BIBLE STUDY",
+          title: "CHRIST-CENTERED LIVING",
+          subtitle: "A study in the book of Colossians",
+          imageUrl: "img/slides/center.jpg",
+          url: "/sermons/series/Christ-Centred%20Living",
+          backgroundPosition: "center",
+          buttonText: "LISTEN AGAIN",
+          internalLink: true,
+        },
+        {
+          type: "GOSPEL MISSION",
+          title: "WHERE IS GOD?",
+          subtitle: "The search for reason in a troubled world",
+          imageUrl: "img/slides/where-is-god.jpeg",
+          url: "/sermons/series/Where%20is%20God%3F",
+          backgroundPosition: "center",
+          buttonText: "LISTEN AGAIN",
+          internalLink: true,
+        },
+        {
+          type: "SUNDAY MORNING SERIES",
+          title: "An Open Letter to the Church",
+          subtitle: "Studies in the Book of Titus",
+          url: "/sermons/series/Titus",
+          imageUrl: "img/slides/titus.jpeg",
+          internalLink: true,
+          buttonText: "LISTEN AGAIN",
+        },
+        {
+          type: "CHILDRENS & YOUTH WORK",
+          title: "What's On",
+          subtitle: "Term-time activities for tots, tweens and teens",
+          url: "/about/whats-on",
+          imageUrl: "img/slides/events.jpg",
+          internalLink: true,
+          buttonText: "FIND OUT MORE",
+        },
+      ],
+    };
   }
 
   getPadding = () => {
-    if($(window).width() > 700) {
-      return ($(window).width() - 700) / 2
+    if ($(window).width() > 700) {
+      return ($(window).width() - 700) / 2;
     } else {
       return 0;
     }
-  }
+  };
 
   updateDimensions = () => {
     this.setState({
-      padding: this.getPadding()
-    })
-  }
+      padding: this.getPadding(),
+    });
+  };
 
   componentDidMount() {
     window.addEventListener("resize", this.updateDimensions);
@@ -65,30 +76,26 @@ class HomePageSlider extends React.Component {
     window.removeEventListener("resize", this.updateDimensions);
   }
 
-  render(){
+  render() {
     var settings = {
       centerMode: true,
-      centerPadding: this.state.padding+'px',
+      centerPadding: this.state.padding + "px",
       slidesToShow: 1,
-      autoplay:true,
-      arrows:false,
-      autoplaySpeed:5000,
-      dots:true
+      autoplay: true,
+      arrows: false,
+      autoplaySpeed: 5000,
+      dots: true,
     };
 
-    const slides = this.state.slides.map((slide,index)=>{
-      return(
+    const slides = this.state.slides.map((slide, index) => {
+      return (
         <div key={index}>
-          <HomePageSlide {...slide}/>
+          <HomePageSlide {...slide} />
         </div>
-      )
-    })
+      );
+    });
 
-    return (
-      <Slider {...settings}>
-        {slides}
-      </Slider>
-    );
+    return <Slider {...settings}>{slides}</Slider>;
   }
 }
 
