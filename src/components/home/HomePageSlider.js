e4c4529e1786dfcac4eb70b67bb62606a7787b5a
Matthew Wilson <matthew.wilson@instil.co>
2025-08-20 18:11:43 +0100

    Removing slides

--- src/components/home/HomePageSlider.js
+++ src/components/home/HomePageSlider.js
@@ -11,24 +11,7 @@ class HomePageSlider extends React.Component {
     super(props);
     this.state = {
       padding: this.getPadding(),
-      slides: [{
-          "type":"REGISTRATION OPEN",
-          "title":"Holiday Bible Club",
-          "subtitle":"9th - 13th June",
-          "imageUrl":"img/slides/hbc.png",
-          "url":"/hbc",
-          "backgroundPosition":"center",
-          "internalLink":true,
-          "buttonText":"Register Now"
-        },{
-          "type":"REGISTRATION OPEN",
-          "title":"Sports 4 Christ",
-          "subtitle": "18th - 21st August",
-          "url":"/s4c",
-          "imageUrl":"img/slides/sports.jpg",
-          "internalLink":true,
-          "buttonText":"REGISTER NOW"
-        },
+      slides: [
         {
           type: "BIBLE STUDY",
           title: "CHRIST-CENTERED LIVING",
